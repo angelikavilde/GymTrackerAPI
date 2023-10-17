61b8f317b027c1d665bdde78118629437364bcfd
angelikavilde <angelikavilde@gmail.com>
2023-10-17 23:45:55 +0100

    removed username

--- workout_db.sql
+++ workout_db.sql
@@ -2,12 +2,11 @@
 CREATE TABLE users(
     user_id SMALLINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     user_code CHAR(12) UNIQUE NOT NULL,
-    username VARCHAR(30) UNIQUE NOT NULL,
     email TEXT UNIQUE NOT NULL
 );
 
-INSERT INTO users(user_code, username, email)
-    VALUES ('admin_user00', 'admin', 'admin');
+INSERT INTO users(user_code, email)
+    VALUES ('admin_user00', 'admin');
 
 CREATE TABLE workout_log(
     workout_id SMALLINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
