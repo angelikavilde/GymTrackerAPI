e5e76c088d485cc5825d0ff2e6a2e8f55b0d3710
angelikavilde <angelikavilde@gmail.com>
2023-10-17 23:03:05 +0100

    added psql table creation

---
+++ workout_db.sql
@@ -0,0 +1,22 @@
+
+CREATE TABLE users(
+    user_id SMALLINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_code CHAR(12) UNIQUE NOT NULL,
+    username VARCHAR(30) UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL
+);
+
+INSERT INTO users(user_code, username, email)
+    VALUES ('admin_user00', 'admin', 'admin');
+
+CREATE TABLE workout_log(
+    workout_id SMALLINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id SMALLINT NOT NULL,
+    "date" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    exercise VARCHAR(60) NOT NULL,
+    "time" SMALLINT,
+    "weight" SMALLINT,
+    "sets" SMALLINT,
+    reps SMALLINT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
