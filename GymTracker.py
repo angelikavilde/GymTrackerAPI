ebdbc1d50747a51ede4d102453f49f9b202cbd2e
angelikavilde <angelikavilde@gmail.com>
2023-10-18 00:12:59 +0100

    added code creation with verification and restoring option

--- GymTracker.py
+++ GymTracker.py
@@ -2,6 +2,7 @@
 
 from secrets import choice
 from string import digits, ascii_letters
+from re import fullmatch
 from os import environ
 
 from dotenv import load_dotenv
@@ -20,15 +21,71 @@ def main_page():
     return send_from_directory("static", "index.html")
 
 
-@app.route("/get_code", methods=["GET"])
-def get_user_code() -> str:
+@app.route("/get_code", methods=["POST"])
+def create_user_code() -> tuple[dict, int]:
     """Creates a code for the user's POST
     request and verifies if it exists"""
-    code = "admin_user00"
+    email = request.args.get('email')
+    if not email:
+        return {"Error": "'email' should be one of the keys for POST request!"}, 400
+    if not verify_email(email):
+        return {"Error": "Email is in the wrong format!"}, 400
     connection = get_db_connection()
+    if check_if_email_exists(connection, email):
+        connection.close()
+        return {"Error": "Email is already in the database! Use /recover_code"}, 409
+    code = "admin_user00"
     while(check_if_code_exists(connection, code)):
         code = generate_unique_code()
-    return str(code)
+    add_user_to_db(connection, code, email)
+    connection.close()
+    # send_email_with_code(code, email)
+    return {"Code": code}, 201
+
+
+@app.route("/recover_code", methods=["POST"])
+def recover_user_code() -> tuple[dict, int]:
+    """"""
+    email = request.args.get('email')
+    if not email:
+        return {"Error": "'email' should be one of the keys for POST request!"}, 400
+    connection = get_db_connection()
+    if not check_if_email_exists(connection, email):
+        connection.close()
+        return {"Error": "Email does not exist! Use /get_code"}, 404
+    code = find_user_code(connection, email)
+    # send_email_with_code(code, email)
+    return {"Code": "Email was sent with your user code associated with the email!"}, 200
+
+
+def find_user_code(conn: connection, email: str) -> str:
+    """"""
+    with conn.cursor() as cur:
+        cur.execute("""SELECT * FROM users WHERE email = %s""", [email])
+        return cur.fetchone()["user_code"]
+
+
+def add_user_to_db(conn: connection, code: str, email: str) -> None:
+    """Adds user's details to the database"""
+    with conn.cursor() as cur:
+        cur.execute("""INSERT INTO users(user_code, email)
+    VALUES (%s, %s);""", [code, email])
+        conn.commit()
+
+
+def check_if_email_exists(conn: connection, email: str) -> bool:
+    """Verifies if the email already exists in the database"""
+    with conn.cursor() as cur:
+        cur.execute("""SELECT * FROM users WHERE email = %s""", [email])
+        data = cur.fetchall()
+    return data != []
+
+
+def verify_email(text: str) -> bool:
+    """Returns True/False depending on if the
+    full text is correctly formatted email"""
+    email_regex = r"\w(?:[-\w+\.]*\w)?@\w[-\w\.]*\.[a-z]+"
+    return bool(fullmatch(email_regex, text.strip()))
 
 
 def get_db_connection() -> connection:
